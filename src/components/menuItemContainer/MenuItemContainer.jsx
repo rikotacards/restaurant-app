69df2130accf2867b6c5678ca52cbedda8f71bd0
rikotacards <max.hsu@bgl.hk>
2019-11-30 14:11:21 +0800

    Make grid columns dynamic based on screen width.

--- src/components/menuItemContainer/MenuItemContainer.jsx
+++ src/components/menuItemContainer/MenuItemContainer.jsx
@@ -1,10 +1,17 @@
 import React from 'react'; 
+import { MenuItem } from 'components/menuItem/MenuItem'
 import './menu-item-container.css'; 
 
 export const MenuItemContainer = (props) => {
+    const { 
+            menuItems,
+            placeHolderData = [1, 2, 3, 4, 5, 6, 7] } = props; 
+    const gridItems = placeHolderData.map((item) => (
+        <MenuItem dishName={item}/>
+    ))
     return (
-        <div className='container'>
-            
+        <div className={`grid-container conditional-columns`}>
+            {gridItems}
         </div>
     )
 }

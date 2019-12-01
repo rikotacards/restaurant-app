06f8eef803468226ae420e70929a581952770dca
rikotacards <max.hsu@bgl.hk>
2019-12-01 09:15:21 +0800

    Set page layout for single restaurant view.

--- src/components/menuItem/MenuItem.jsx
+++ src/components/menuItem/MenuItem.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import './menu-item.css';
+import { Card } from 'components/card/Card'
 
 export const MenuItem = props => {
     //To do, pass in props
     const { dishName } = props; 
   return (
-   <div className='container'>
-       <div className='title'>Pad Thai</div>
-       <hr className='divider'/>
+   <Card>
        <div className='image'>IMAGE</div>
-       <hr className='divider'/>
+       <div className='title'>Pad Thai</div>
        <div className='description'> insert desc</div>
        <button className='menu-item-button'>Add to order </button>
-   </div>
+    </Card>
   );
 };
